56c4e9acd4395ac300699d220fbeff36100d8b85
Lirolla <contato@lirolla.com>
2026-02-18 12:27:04 -0500

    Add console logs

--- index.tsx
+++ index.tsx
@@ -3,14 +3,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+console.log('Index.tsx loading...');
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
+  console.error('Root element not found!');
   throw new Error("Could not find root element to mount to");
 }
 
+console.log('Root element found, creating React root...');
+
 const root = ReactDOM.createRoot(rootElement);
+
+console.log('React root created, rendering App...');
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+console.log('App rendered successfully!');
